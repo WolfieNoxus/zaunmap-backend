76cdad8221da09fe4e5b14cd74158bb5378b1cc1
zhangwen-hu <Zhangwen.Hu01@gmail.com>
2023-11-07 15:30:41 -0500

    small changes

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-/*
-    This is our back-end server, which employs some middleware
-    to make sure data is received in the proper format (i.e. JSON)
-    and hooks up all of our pieces.
-*/
-
 // THESE ARE NODE APIs WE WISH TO USE
 const express = require('express')
 const cors = require('cors')
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
 
 // CREATE OUR SERVER
+dotenv.config()
+const PORT = process.env.PORT || 4000;
 const app = express()
+
+// SETUP THE MIDDLEWARE
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 // SETUP OUR OWN ROUTERS AS MIDDLEWARE
-const playlistRouter = require('./routes/playlists-router')
-app.use('/api', playlistRouter)
+// const authRouter = require('./routes/auth-router')
+// app.use('/auth', authRouter)
+const playlistsRouter = require('./routes/playlists-router')
+app.use('/api', playlistsRouter)
 
 // INITIALIZE OUR DATABASE OBJECT
 const db = require('./db')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 // PUT THE SERVER IN LISTENING MODE
-const apiPort = 4000
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
