e6f5c88030e0e1ba491e1d574eb7e094ef018c5e
zhangwen-hu <Zhangwen.Hu01@gmail.com>
2023-11-27 23:53:44 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const mapRoutes = require('./routes/mapRoutes');
@@ -7,10 +8,21 @@ const commentRoutes = require('./routes/commentRoutes');
 
 const app = express();
 if (process.env.NODE_ENV !== 'test') {
-    connectDB();
+  connectDB();
 }
 
 app.use(express.json());
+
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", '*');
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//   next();
+// });
+
+app.use(cors());
 
 // Add a route for /api/hello
 app.get('/api/hello', (req, res) => {
