1bc49421b2bad6fb798c52e1c644ad022e9f9a1c
zhangwen-hu <Zhangwen.Hu01@gmail.com>
2023-11-29 23:40:24 -0500

    like and dislike added

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -6,5 +6,7 @@ router.get('/', CommentController.getComment);
 router.delete('/', CommentController.deleteComment);
 router.post('/', CommentController.createComment);
 router.post('/reply', CommentController.replyComment);
+router.post('/like', CommentController.likeComment);
+router.post('/dislike', CommentController.dislikeComment);
 
 module.exports = router;
