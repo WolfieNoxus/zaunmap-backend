dd7b58ba291224625ae6a2dd566a57bd6dfcf5f9
zhangwen-hu <Zhangwen.Hu01@gmail.com>
2023-12-05 03:44:32 -0500

    try fix again

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect("mongodb+srv://our-first-user:MJhshHlUpoOAuYgb@zaunmap.lgbnudq.mongodb.net/?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
