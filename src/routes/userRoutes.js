28a5f1b355f32f6aea97bc0962d4279372188e45
zhangwen-hu <Zhangwen.Hu01@gmail.com>
2023-12-05 02:55:52 -0500

    deplyment issue

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const { checkJwt, requiredScopes } = require('../config/auth');
+const { checkJwt, checkScopes } = require('../config/auth');
 
 router.get('/', userController.getUser);
 router.get('/search', userController.searchUsers);
